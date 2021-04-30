0f49b19027f29eb9140ab729c1994a9c250753ab
Ancoder2009 <68029104+Ancoder2009@users.noreply.github.com>
2021-04-30 12:14:48 -0700

    Update testforscrblx-1.0.0.js

--- testforscrblx-1.0.0.js
+++ testforscrblx-1.0.0.js
@@ -1,64 +1,31 @@
-console.log("Loading testforscrblx-1.0.0");
-console.log("Loading game engine!");
-
-var user
-
-class gameengine {
-	constructor(runtime, extensionId) {
-		this.runtime = runtime;
-	}
-	
-	getinfo(){
-		return {
-			id: 'gameengine',
-			name: 'Game Engine',
-			blocks: [
-				{
-					opcode: 'uploadgame',
-					blockType: Scratch.BlockType.COMMAND,
-					text: 'Upload a game to server: [server] With password: [password] data=[data]',
-					arguments: {
-						server: {
-							type: Scratch.ArgumentType.STRING,
-							defaultValue: 'https://server.example.com'
-						},
-						
-						password: {
-							type: Scratch.ArgumentType.STRING,
-							defaultValue: 'PSWD'
-						},
-						
-						data: {
-							type: Scratch.ArgumentType.STRING,
-							defaultValue: 'DATA'
-
-						}
-					}
-				}
-			] 
-		}
-	}
-	uploadgame(args) {
-		fetch(args.server, {
-    			method: 'post',
-    			headers: {
-      				"Content-type": "application/json;"
-    			},
-    			body: {
-				"data": args.data,
-				"password": args.password,
-			}
-  		})
-  		.then(json)
-  		.then(function (data) {
-		 console.log('Request succeeded with JSON response', data);
-  		})
-		.catch(function (error) {
-    		 console.log('Request failed', error);
-  		});
-		return data
-		
-	}
+class SaveEngine {
+    constructor (runtime) {
+        this.runtime = runtime;
+    }
+    getInfo () {
+        return {
+            id: 'saveengine',
+            name: 'Save Engine',
+            blocks: [
+                {
+                    opcode: 'save',
+                    blockType: Scratch.BlockType.COMMAMD,
+                    text: 'Send [data] to [server]',
+                    arguments: {
+                        data: {
+                            type: ArgumentType.STRING,
+                            defaultValue: '1'
+                        },
+                        server: {
+                            type: ArgumentType.STRING,
+                            defaultValue: 'text'
+                        }
+                    }
+                }
+            ]
+        };
+    }
+    save(args) {
+    
+    }
 }
-
-Scratch.extensions.register(new gameengine());
